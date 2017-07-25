be1b72f6b96220a26bb54eccc69248b21e83a9c3
GydeonMylls <GydeonMylls@users.noreply.github.com>
2017-07-25 17:56:17 +0100

    Transferred questions from dialogs to normal fragments, added device and course IDs

--- app/src/main/java/uk/ac/cam/gsm31/inspiringfutures/ESM/ESM_Text.java
+++ app/src/main/java/uk/ac/cam/gsm31/inspiringfutures/ESM/ESM_Text.java
@@ -16,11 +16,11 @@
 
 package uk.ac.cam.gsm31.inspiringfutures.ESM;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -36,40 +36,31 @@ public class ESM_Text extends ESM_Question {
 
     public static final String TAG = "ESM_Text";
 
-    public static final String esm_layout = "esm_text";
+    private TextView mQuestion;
+    private EditText mResponse;
 
+    @Nullable
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-
-        View view = getViewByName(esm_layout);
-
-        // Can't use dialog title as it cuts off long strings
-        TextView esm_question = view.findViewById(R.id.esm_question);
-        esm_question.setText(question());
-
-        TextView esm_instructions = view.findViewById(R.id.esm_instructions);
-        esm_instructions.setText(instructions());
-
-        final EditText esm_response = view.findViewById(R.id.esm_response);
-
-        Dialog dialog = new AlertDialog.Builder(getActivity())                          // TODO Maybe move dialog creation to ESM_Question
-                .setView(view)
-                .setPositiveButton(isLast() ? "Submit" : "Next", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        getListener().receiveResponse(esm_response.getText().toString());
-                    }
-                })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        getListener().receiveCancel();
-                    }
-                })
-                .create();
-        dialog.setCanceledOnTouchOutside(false);
-
-        return dialog;
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.esm_text, container, false);       // Hopefully this will get moved to ESM_Question
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Can't use dialog title as it cuts off long strings
+        mQuestion = view.findViewById(R.id.esm_question);
+        mQuestion.setText(question());
+
+        mResponse = view.findViewById(R.id.esm_response);
+        mResponse.setHint(instructions());
+    }
+
+    @Override
+    public String getResponse() {
+        if (mResponse != null) {
+            return mResponse.getText().toString();
+        } else { return null; }
+    }
 }
